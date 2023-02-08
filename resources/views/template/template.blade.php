cfa18e4470bb24a48ee475401b5588998324d148
Tetuko <namaku775211@gmail.com>
2023-02-08 13:51:40 +0700

    push logo

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta name="author" content="INSPIRO" />
     <meta name="description" content="Themeforest Template Polo, html template">
-    <link rel="icon" type="image/png" href="images/favicon.png">
+    <link rel="icon" type="image/png" href="images/hi-logo.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- Document title -->
     <title>Hubungan International Universitas Jember</title>
@@ -18,8 +18,98 @@
 <body>
     <!-- Body Inner -->
     <div class="body-inner">
-@yield('header')
+{{-- @yield('header') --}}
+        <!-- Header -->
         
+<header id="header" data-transparent="true" class="dark">
+    <div class="header-inner">
+        <div class="container">
+            <!--Logo-->
+            <div id="logo">
+                <a href="{{route('home')}}">
+                    
+                </a>
+                <img class="" src="images/hi-logo-removebg.png" alt="" width="120px" height="120px" style="margin-bottom: 100%"> 
+            </div>
+            
+            <!--End: Logo-->
+            <!-- Search -->
+            <div id="search"><a id="btn-search-close" class="btn-search-close" aria-label="Close search form"><i class="icon-x"></i></a>
+                <form class="search-form" action="search-results-page.html" method="get">
+                    <input class="form-control" name="q" type="text" placeholder="Type & Search..." />
+                    <span class="text-muted">Start typing & press "Enter" or "ESC" to close</span>
+                </form>
+            </div>
+            <!-- end: search -->
+            <!--Header Extras-->
+            <div class="header-extras">
+                <ul>
+                    <li>
+                        
+                        <a id="btn-search" href="#"> <i class="icon-search"></i></a>
+                    </li>
+                </ul>
+            </div>
+            <!--end: Header Extras-->
+            <!--Navigation Resposnive Trigger-->
+            <div id="mainMenu-trigger"> <a class="lines-button x"><span class="lines"></span></a> </div>
+            <!--end: Navigation Resposnive Trigger-->
+            <!--Navigation-->
+            <div id="mainMenu">
+                <div class="container">
+                    <nav>
+                        <ul>
+                            
+                            <li><a href="{{route('home')}}">Home</a></li>
+                            <li class="dropdown"><a href="{{route('profile')}}">Profile</a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="">Introduction</a>
+                                    </li>
+                                    <li><a href="{{route('staff')}}">Lecture & Staff</a>
+                                    </li>
+                                    <li class="dropdown-submenu"><a href="{{route('structure-organization')}}">Student Corner</a>
+                                        <ul class="dropdown-menu">
+                                            <li><a href="#">Himahi</a>
+                                            </li>
+                                            <li><a href="#">Activities</a>
+                                            </li>
+                                            <li><a href="#">Student's Achievment Submission</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="{{route('facillitas')}}">Facilitas</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a href="{{route('academic')}}">Academic</a></li>
+                            <li class="dropdown"><a href="#">Publication And Research</a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Book</a>
+                                    </li>
+                                    <li><a href="#">Article/Proceeding</a>
+                                    </ul>
+                                </li>
+                                <li class="dropdown"><a href="#">News</a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="{{route('publication-research')}}">Student</a>
+                                        </li>
+                                        <li><a href="{{route('testimony')}}">Lecturer</a>
+                                        </li>
+                                        <li><a href="{{route('press-release')}}">Press Release</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Testimony</a></li>
+                                <li><a href="{{route('download')}}">Download</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <!--end: Navigation-->
+            </div>
+        </div>
+    </header>
+    <!-- end: Header -->
 @yield('hover')
 
 @yield('content')
